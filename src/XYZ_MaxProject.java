cfbb3cefb65d515b48dabd419e3269cb0a7a2aa3
Kota@mac <miura@embl.de>
2012-02-16 23:45:44 +0100

    added XYZ_MaxProject, does 3D stack.

---
+++ src/XYZ_MaxProject.java
@@ -0,0 +1,20 @@
+import emblcmci.XYZMaxProject;
+import ij.IJ;
+import ij.ImagePlus;
+import ij.plugin.PlugIn;
+
+public class XYZ_MaxProject implements PlugIn {
+
+	@Override
+	public void run(String arg) {
+		ImagePlus imp = IJ.getImage();
+		if (imp != null){
+			XYZMaxProject mp = new XYZMaxProject(imp);
+			ImagePlus impxyz = mp.getXYZProject();
+			impxyz.show();
+		} else {
+			IJ.log("no image, as it seems...");
+		}
+	}
+
+}
